ce4bee3dbc399297e48b2363a18dad99e10f52d9
franco <francohuanaco@gmail.com>
2020-02-10 06:37:45 -0400

    Display recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
       "a Test Recipe",
       "this is simply a test",
       "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg"
+    ),
+    new Recipe(
+      "a Test Recipe",
+      "this is simply a test",
+      "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg"
     )
   ];
   constructor() {}
